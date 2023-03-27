6e85a7db23c4f9798bd814913e8faa845f25606a
aliLtRP <albaghdadyali1@gmail.com>
2023-03-27 16:43:06 +0300

    calculations done

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 import * as myFunc from "./helpers.js";
 
 const button = document.querySelector('button');
+let from = document.getElementById('from');
 
 // calc result
 button.addEventListener('click', () => {
     let data = myFunc.value_inp[0].value;
     let data2 = myFunc.value_inp[1].value;
 
-    if ((myFunc.isBinary(data) && myFunc.isBinary(data2)) == true) {
-        myFunc.result_Bin(data, data2);
-    };
+    let op = document.getElementById('select').value;
 
+    // final result
+    myFunc.result(data, data2, op, from.value);
 });
 
 
 // validate that input is not text, unless it's hex
-const from = document.getElementById('from');
 
 from.addEventListener('change', () => {
-    myFunc.inputValidationInputOne();
-    myFunc.inputValidationInputTwo();
+    myFunc.inputValidationInput(from.value);
 });
--- js/helpers.js
+++ js/helpers.js
@@ -2,92 +2,98 @@ export const value_inp = document.querySelectorAll('input');
 export const texrArea = document.querySelector('textarea');
 
 
-// check if the number is in binary form or not
-export function isBinary(num) {
-    let result = false;
+// result calc. base important to know the calculation and result will be in what form
+export const result = (data, data2, op, from) => {
+    let result;
+    let base = parseInt(from);
 
-    for(let i=0; i< num.length; i++){
-        if (num[i] == "0" || num[i] == "1") {
-            result = true;
-        } else {
-            result = false;
-        }
-    }
+    data = parseInt(data, base);
+    data2 = parseInt(data2, base);
 
-    return result;
-}
+    switch (op) {
 
+        case '+':
+            result = (data + data2).toString(base);
+            break;
+        case '-':
+            result = (data - data2).toString(base);
+            break;
+        case 'x':
+            result = (data * data2).toString(base);
+            break;
+        default:
+            result = (data / data2).toString(base);
+    };
 
-// get the operator
-export let operator = document.getElementById('select').value;
-
-// binary calculations
-export const result_Bin = (input_1, input_2) => {
-    const size_1 = input_1.length;
-    const size_2 = input_2.length;
-    const size = size_1 >= size_2 ? size_1 : size_2;
-
-    if (operator == '+') {
-        texrArea.textContent = (parseInt(input_1, 2) + parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    } else if (operator == '-'){
-         texrArea.textContent = (parseInt(input_1, 2) - parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    } else if (operator == 'x') {
-         texrArea.textContent = (parseInt(input_1, 2) * parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    } else {
-         texrArea.textContent = (parseInt(input_1, 2) / parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    }
-    
+    return texrArea.textContent = result;
 };
 
+
+
+// check if the number is in binary form or not
+export function isBinary(data) {
+    let result = data.match(/^[0-1]+$/) ? true : false;
+    return result;
+};
+
+export function isDec(data) {
+    let result = data.match(/^[0-9]*$/) ? true : false;
+    return result;
+}
 
 // check if number in hex form 
 export function isHex(data) {
-    let result = data.match(/^[0-9a-f]+$/i) ? true:false;
+    let result = data.match(/^[0-9a-f]+$/i) ? true : false;
     return result;
-}
-
-
-
-export function inputValidationInputOne(){
-    const valEnt = value_inp[0];
-
-    if (from.value === 'Hex') {
-        // get input tag
-        let inp_1 = document.getElementById('input1');
-
-        // if input field get change
-        inp_1.addEventListener('input', () => {
-
-            // change type of input from number to text
-            valEnt.setAttribute('type', 'text');
-
-            // ensure the text is in hex numbers range 0-f
-            if (isHex(inp_1.value) != true) {
-                const s = inp_1.value;
-                inp_1.value = s.substr(0, s.length - 1);
-            };
-
-        });
-    };
 };
 
 
-export function inputValidationInputTwo(){
+// octal validation
+export function isOctal(data) {
+    let result = data.match(/^[0-8]+$/i) ? true : false;
+    return result;
+};
 
+
+// check if input is in valid form for each numbers system
+export function inputValidationInput(base) {
+
+    switch (base) {
+        case '16':
+            dataCheck(isHex);
+            break;
+        case '8':
+            dataCheck(isOctal);
+        case '10':
+            dataCheck(isDec);
+            break;
+        default:
+            dataCheck(isBinary);
+    }
+};
+
+function dataCheck(sysNum) {
+    const valEnt = value_inp[0];
     const valEnt2 = value_inp[1];
-
-    if (from.value === 'Hex') {
-        const getData = document.getElementById('input2');
-
-        getData.addEventListener('input', ()=>{
-            valEnt2.setAttribute('type', 'text');
-
-            if (isHex(getData.value) != true) {
-                const s = getData.value;
-                getData.value = s.substr(0, s.length - 1);
-            };
-            
-        });   
+    const getData = document.getElementById('input1');
+    const getData2 = document.getElementById('input2');
     
-    };
+    getData2.addEventListener('input', () => {
+        valEnt2.setAttribute('type', 'text');
+        
+        if (sysNum(getData2.value) != true) {
+            const s = getData2.value;
+            getData2.value = s.substr(0, s.length - 1);
+        };
+    });
+
+    getData.addEventListener('input', () => {
+        valEnt.setAttribute('type', 'text');
+        
+        if (sysNum(getData.value) != true) {
+            const s = getData.value;
+            getData.value = s.substr(0, s.length - 1);
+        };
+    });
+
 };
