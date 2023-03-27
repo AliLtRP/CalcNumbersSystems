22990c59c5b8320edb2540dc720488a5852304c0
aliLtRP <albaghdadyali1@gmail.com>
2023-03-27 14:35:22 +0300

    input validation

--- js/app.js
+++ js/app.js
@@ -1,37 +1,23 @@
-import { isBinary } from "./helpers.js";
+import * as myFunc from "./helpers.js";
 
-let operator;
-const value_inp = document.querySelectorAll('input');
 const button = document.querySelector('button');
-const texrArea = document.querySelector('textarea');
 
+// calc result
 button.addEventListener('click', () => {
-    let data = value_inp[0].value;
-    let data2 = value_inp[1].value;
+    let data = myFunc.value_inp[0].value;
+    let data2 = myFunc.value_inp[1].value;
 
-    operator = document.querySelector('select').value;
+    if ((myFunc.isBinary(data) && myFunc.isBinary(data2)) == true) {
+        myFunc.result_Bin(data, data2);
+    };
 
-    if ((isBinary(data) && isBinary(data2) ) == true) {
-        result_Bin(data, data2);
-    } 
-    result_Bin(data, data2);
 });
 
 
-// binary calculations
-const result_Bin = (input_1, input_2) => {
-    const size_1 = input_1.length;
-    const size_2 = input_2.length;
-    const size = size_1 >= size_2 ? size_1 : size_2;
+// validate that input is not text, unless it's hex
+const from = document.getElementById('from');
 
-    if (operator == '+') {
-        texrArea.textContent = (parseInt(input_1, 2) + parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    } else if (operator == '-'){
-         texrArea.textContent = (parseInt(input_1, 2) - parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    } else if (operator == 'x') {
-         texrArea.textContent = (parseInt(input_1, 2) * parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    } else {
-         texrArea.textContent = (parseInt(input_1, 2) / parseInt(input_2, 2)).toString(2).padStart(size, '0');
-    }
-    
-};
+from.addEventListener('change', () => {
+    myFunc.inputValidationInputOne();
+    myFunc.inputValidationInputTwo();
+});
--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,7 @@
+export const value_inp = document.querySelectorAll('input');
+export const texrArea = document.querySelector('textarea');
+
+
 // check if the number is in binary form or not
 export function isBinary(num) {
     let result = false;
@@ -13,7 +17,77 @@ export function isBinary(num) {
     return result;
 }
 
-// check if number in hex form 
-function isHex() {
 
+// get the operator
+export let operator = document.getElementById('select').value;
+
+// binary calculations
+export const result_Bin = (input_1, input_2) => {
+    const size_1 = input_1.length;
+    const size_2 = input_2.length;
+    const size = size_1 >= size_2 ? size_1 : size_2;
+
+    if (operator == '+') {
+        texrArea.textContent = (parseInt(input_1, 2) + parseInt(input_2, 2)).toString(2).padStart(size, '0');
+    } else if (operator == '-'){
+         texrArea.textContent = (parseInt(input_1, 2) - parseInt(input_2, 2)).toString(2).padStart(size, '0');
+    } else if (operator == 'x') {
+         texrArea.textContent = (parseInt(input_1, 2) * parseInt(input_2, 2)).toString(2).padStart(size, '0');
+    } else {
+         texrArea.textContent = (parseInt(input_1, 2) / parseInt(input_2, 2)).toString(2).padStart(size, '0');
+    }
+    
+};
+
+
+// check if number in hex form 
+export function isHex(data) {
+    let result = data.match(/^[0-9a-f]+$/i) ? true:false;
+    return result;
 }
+
+
+
+export function inputValidationInputOne(){
+    const valEnt = value_inp[0];
+
+    if (from.value === 'Hex') {
+        // get input tag
+        let inp_1 = document.getElementById('input1');
+
+        // if input field get change
+        inp_1.addEventListener('input', () => {
+
+            // change type of input from number to text
+            valEnt.setAttribute('type', 'text');
+
+            // ensure the text is in hex numbers range 0-f
+            if (isHex(inp_1.value) != true) {
+                const s = inp_1.value;
+                inp_1.value = s.substr(0, s.length - 1);
+            };
+
+        });
+    };
+};
+
+
+export function inputValidationInputTwo(){
+
+    const valEnt2 = value_inp[1];
+
+    if (from.value === 'Hex') {
+        const getData = document.getElementById('input2');
+
+        getData.addEventListener('input', ()=>{
+            valEnt2.setAttribute('type', 'text');
+
+            if (isHex(getData.value) != true) {
+                const s = getData.value;
+                getData.value = s.substr(0, s.length - 1);
+            };
+            
+        });   
+    
+    };
+};
